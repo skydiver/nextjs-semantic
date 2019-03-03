ce70ab121c5b4c7d4b2e5fdf5e87bfaa899861d8
Martín M <skydiver@users.noreply.github.com>
2019-03-03 18:34:08 -0300

    nextjs config

---
+++ next.config.js
@@ -0,0 +1,25 @@
+const withPlugins = require('next-compose-plugins');
+const css = require('@zeit/next-css');
+
+const nextConfig = {
+  target: 'serverless',
+  webpack (config) {
+    config.module.rules.push({
+      test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+          publicPath: '/_next/static/',
+          outputPath: 'static/',
+          name: '[name].[ext]'
+        }
+      }
+    });
+    return config;
+  }
+};
+
+module.exports = withPlugins([
+  [css],
+], nextConfig);
