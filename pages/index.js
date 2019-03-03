47ad1d81beec4c7616bcb48a0d683edb08f34e00
Martín M <skydiver@users.noreply.github.com>
2019-03-03 18:45:57 -0300

    layout + basic page

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { Button } from 'semantic-ui-react';
+
+const StyledContainer = styled.div`
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Home = () =>
+  <StyledContainer>
+    <div>
+      <h1>Next.js + Semantic UI!</h1>
+    </div>
+    <div>
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
+    </div>
+  </StyledContainer>
+;
+
+export default Home;
