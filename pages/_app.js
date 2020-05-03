02219efacf90c1852b75fb2938664c6465ea7289
Martin M <skydiver@users.noreply.github.com>
2020-05-03 00:35:31 -0300

    Release 2.1.0 (#13)
    * updated dependencies
    
    * removed deprecated code
    
    * version bump

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import App, { Container } from 'next/app';
+import App from 'next/app';
 
 import '../.semantic/dist/semantic.min.css';
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
-
-    return (
-      <Container>
-        <Component {...pageProps} />
-      </Container>
-    );
+    return <Component {...pageProps} />;
   }
 }
 
