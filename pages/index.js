6f317d0298b518b308856630b2bb8644e5da61ea
Martín M <skydiver@users.noreply.github.com>
2020-09-30 11:17:27 -0300

    added icons to example page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Button } from 'semantic-ui-react';
+import { Button, Icon } from 'semantic-ui-react';
 
 const StyledContainer = styled.div`
   display: flex;
@@ -7,6 +7,10 @@ const StyledContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  div {
+    margin: 10px 0 10px 0;
+  }
 `;
 
 const Home = () => (
@@ -17,6 +21,13 @@ const Home = () => (
     <div>
       <Button primary>Primary</Button>
       <Button secondary>Secondary</Button>
+    </div>
+    <div>
+      <Icon name="home" size="big" />
+      <Icon name="star" size="big" />
+      <Icon name="heart" size="big" />
+      <Icon name="ambulance" size="big" />
+      <Icon name="lightbulb" size="big" />
     </div>
   </StyledContainer>
 );
